5b573755278c243b767c7b6fd46015b28927317f
aniketdharmik <aniket.dharmik@ibm.com>
2022-03-04 18:50:26 -0500

    router changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationFormComponent } from './user-registration/registration-form/registration-form.component';
 import { UserProfileComponent } from './user-registration/user-profile/user-profile.component';
+import { LoginFormComponent } from './user-registration/login-form/login-form.component';
 
 const routes: Routes = [
+  {path:'', component:LoginFormComponent},
   {path:'RegistrationForm', component:RegistrationFormComponent},
   {path:'UserProfile', component:UserProfileComponent}
 ];
