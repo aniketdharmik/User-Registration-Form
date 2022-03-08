8fca4a8191ce0b74e74ce867c8c90547648aadd5
aniketdharmik <aniket.dharmik@ibm.com>
2022-03-08 16:48:18 -0500

    auth guard module, validations and unit test

--- src/app/user-registration/login-form/login-form.component.spec.ts
+++ src/app/user-registration/login-form/login-form.component.spec.ts
@@ -1,13 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { LoginFormComponent } from './login-form.component';
+
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {  MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
+
 
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[RouterTestingModule, FormsModule,ReactiveFormsModule, MatFormFieldModule, MatInputModule, BrowserAnimationsModule],
       declarations: [ LoginFormComponent ]
     })
     .compileComponents();
@@ -27,7 +36,16 @@ describe('LoginFormComponent', () => {
     expect(component.email.valid).toBeFalsy();
   });
 
-  it('email input valid when has valid entry', () => {
-    expect(component.email.valid).toBeTrue();
+  it('Password input field is visible when clicked on password visble icon', () =>{
+      expect(component.showPasswordVisibility).toBeTruthy();
   });
+
+  it('Check if Mat Icon button clicked is called in LoginFormComponent', fakeAsync(() => {
+    spyOn(component, 'showPasswordVisibility');
+    let button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    tick();
+    expect(component.showPasswordVisibility).toHaveBeenCalled();
+  }));
+  
 });
